5393ea2bec7f8a742305d32215a6292637da216a
aya ehab <ayaaehab1999@gmail.com>
2024-10-22 16:12:41 +0300

    add index

--- controllers/applications.js
+++ controllers/applications.js
@@ -7,11 +7,39 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
     try {
-      res.render('applications/index.ejs');
+    const currentUser = await User.findById(req.session.user._id);
+
+    res.render('applications/index.ejs', {applications: currentUser.applications,});
     } catch (error) {
       console.log(error)
       res.redirect('/')
     }
-  });
+});
+
+router.get('/new', async (req, res) => {
+  res.render('applications/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+
+    // Modify the date part of the form
+    req.body.date = new Date(req.body.date)
+
+    // Push req.body (the new form data object) to the
+    // applications array of the current user
+    currentUser.applications.push(req.body);
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/')
+  }
+});
 
 module.exports = router;
